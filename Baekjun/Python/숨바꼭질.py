fa9a5ae64f192a6ee9cb72a006450a02b68e1a5c
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2021-07-22 15:32:04 +0900

    Update 숨바꼭질.py

--- Baekjun/Python/숨바꼭질.py
+++ Baekjun/Python/숨바꼭질.py
@@ -17,24 +17,44 @@ me, sister = input().split()
 me = int(me)
 sister = int(sister)
 
-    
+out  = False
 cnt = 0
-visited = [ me ]
 q = deque()
-
 q.append(me)
 
+visited = dict()
+
 while 1:
-    me = q.popleft()
-    if len(q) == 0:
-        cnt += 1
-    visited.append(me)
-    if me < sister :
-        # +, /
-        print()
-    elif me > sister :
-        # -
-        print()
-    else:
-        print(cnt)
+    arr_temp = []
+    while len(q) != 0 :
+        me = q.popleft()
+        visited[me] = 1
+        if me == sister:
+            out = True
+            break
+        if me+1 == sister or me-1 == sister or me*2 == sister:
+            out = True
+            cnt += 1
+            break
+
+        if me != 0 :
+            if me-1 not in visited :
+                arr_temp.append(me-1)
+            if me < sister:
+                if me*2 not in visited :
+                    arr_temp.append(me*2)
+                if me+1 not in visited :
+                    arr_temp.append(me+1)
+        else:
+            if me+1 not in visited :
+                arr_temp.append(me+1)
+        
+        if len(q) == 0:
+            for i in range(0, len(arr_temp)):
+                q.append(arr_temp[i])
+            arr_temp = []
+            cnt += 1
+    if out == True:
         break
+    
+print(cnt)
